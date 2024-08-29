7b64abfd43487646ca8b90af375808a2bb05ca45
Sparrows <35826584+titan2762@users.noreply.github.com>
2024-08-29 13:46:24 +0800

    planning

---
+++ flashcards app.py
@@ -0,0 +1,10 @@
+# making a flashcard application to quiz users on questions
+
+#store flash cards
+#present flashcards randomly
+# accept answers multichoice
+#check if answer is correct for the flashcard.
+#user can change what flashcard is displayd by "pass", or if commplete auto next
+#keep score or correct vs total numbers.
+#option to replay question we got wrong.
+

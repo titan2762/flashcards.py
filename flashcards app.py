3d811c42dbdaafbdcfe0955bfc092563dfa8fff5
Sparrows <35826584+titan2762@users.noreply.github.com>
2024-08-31 20:19:15 +0800

    funny

--- flashcards app.py
+++ flashcards app.py
@@ -30,6 +30,10 @@ flashcard_answers = ["1", "2", "3"]
 flashcard_incorrect = []
 
 def maincards(flashcards):
+
+    correct_answers_crate1 = []
+    correct_answers_crate2 = []
+
     global flashcard_incorrect, correct_answers, total_displayed
     correct_answers = 0
     total_displayed = 0
@@ -43,39 +47,46 @@ def maincards(flashcards):
         for card in flashcards:  # goes through cards and counts which card it is at
             index += 1
             if card == displayed:
-                print(flashcard_answers[index])
-                total_displayed += 1
-                
-                # prompt
-                answer = input("... IN MAIN ...What is the answer? (type 'quit' to exit) >> ")
-                
-                if answer.lower() == "rev":
-                    print("going to review incorrect answers")
-                    incorrectcards()
-
-                if answer.lower() == "quit":
-                    total_displayed -= 1
-                    print(correct_answers)
-                    print(total_displayed)
-                    print("Quitting the flashcards program.")
-                    return flashcard_incorrect, correct_answers # Exits the function
-
-                if answer.lower() == "pass":
-                    total_displayed -= 1
-                    print("Passing\n")
+                if card in correct_answers_crate2:
                     continue
-
-                # correct answer sequence
-                if flashcard_answers[index] == answer:
-                    correct_answers += 1
-                    print("Good job!\n")
-                    continue
-                
-                # incorrect answer sequence
                 else:
-                    flashcard_incorrect.append(displayed)
-                    print("Nice try\n")
-                    continue                
+                    print(flashcard_answers[index])
+                    total_displayed += 1
+                
+                    # prompt
+                    answer = input("... IN MAIN ...What is the answer? (type 'quit' to exit) >> ")
+                
+                    if answer.lower() == "rev":
+                        print("going to review incorrect answers")
+                        incorrectcards()
+
+                    if answer.lower() == "quit":
+                        total_displayed -= 1
+                        print(correct_answers)
+                        print(total_displayed)
+                        print("Quitting the flashcards program.")
+                        return flashcard_incorrect, correct_answers # Exits the function
+
+                    if answer.lower() == "pass":
+                        total_displayed -= 1
+                        print("Passing\n")
+                        continue
+
+                    # correct answer sequence
+                    if flashcard_answers[index] == answer:
+                        correct_answers += 1
+                        print("Good job!\n")
+                        if card in correct_answers_crate1:
+                            correct_answers_crate2.append(card)
+                        else:
+                            correct_answers_crate1.append(card)
+                        continue
+                
+                    # incorrect answer sequence
+                    else:
+                        flashcard_incorrect.append(displayed)
+                        print("Nice try\n")
+                        continue                
             else:
                 continue
 
